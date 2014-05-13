4414b8c2565d6edac33c207343496bad62758b96
Artem Ivantsov <loky@Artems-MacBook-Pro.local>
2014-05-13 09:09:36 +0300

    fix recipes/add-solr-support.rb

--- recipes/add-solr-support.rb
+++ recipes/add-solr-support.rb
@@ -4,24 +4,24 @@ service "tomcat" do
   action :nothing
 end
 
-directory "#{node['build']['target']}/WEB-INF" do 
+directory "#{node['cookbook-qubell-build']['target']}/WEB-INF" do 
   action :create
 end
 
-template "#{node['build']['target']}/WEB-INF/applicationContext.xml" do
+template "#{node['cookbook-qubell-build']['target']}/WEB-INF/applicationContext.xml" do
       source "applicationContext.xml.erb"
       variables({
-        :solr_url => node["broadleaf"]["solr_url"],
-        :solr_reindex_url => node["broadleaf"]["solr_reindex_url"]
+        :solr_url => node["cookbook-qubell-broadleaf"]["solr_url"],
+        :solr_reindex_url => node["cookbook-qubell-broadleaf"]["solr_reindex_url"]
       })
     end
 execute "upadte_war" do
-    cwd "#{node['build']['target']}"
+    cwd "#{node['cookbook-qubell-build']['target']}"
     command "jar -uvf mycompany.war WEB-INF/applicationContext.xml"
     action :run
 end
 
-directory "#{node['build']['target']}/WEB-INF" do 
+directory "#{node['cookbook-qubell-build']['target']}/WEB-INF" do 
   action :delete
   recursive true
 end
