97e4f0cb73e4c125f9b7b4a68b60a33bf9ca201e
Artem Ivantsov <aivantsov@qubell.com>
2015-03-31 15:32:10 +0300

    check war changes before patch

--- recipes/add-solr-support.rb
+++ recipes/add-solr-support.rb
@@ -10,16 +10,19 @@ template "#{node['cookbook-qubell-build']['target']}/WEB-INF/applicationContext.
         :solr_reindex_url => node["cookbook-qubell-broadleaf"]["solr_reindex_url"]
       })
     end
+
 execute "upadte_war" do
     cwd "#{node['cookbook-qubell-build']['target']}"
     command "jar -uvf mycompany.war WEB-INF/applicationContext.xml"
     action :run
+    not_if "unzip -p /tmp/mvn/mycompany.war* WEB-INF/applicationContext.xml| grep -q #{node['cookbook-qubell-broadleaf']['solr_url']}"
+    notifies :run, "execute[backup_template]", :immediately
 end
 
 execute "backup_template" do
     cwd "#{node['cookbook-qubell-build']['target']}"
     command "cp -f WEB-INF/applicationContext.xml /tmp"
-    action :run
+    action :nothing
 end
 
 directory "#{node['cookbook-qubell-build']['target']}/WEB-INF" do 
